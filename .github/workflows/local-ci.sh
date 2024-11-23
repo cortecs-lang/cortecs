59301afc5308aa34e680c4ac3c92e9928e75734d
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2024-11-22 20:38:52 -0800

    Refactor code structure to have test in the actual package

--- .github/workflows/local-ci.sh
+++ .github/workflows/local-ci.sh
@@ -5,7 +5,6 @@ rm compile_commands.json
 
 # verify file formatting
 find source/ -iname '*.h' -o -iname '*.c' -exec clang-format --dry-run --Werror {} \;
-find test/ -iname '*.h' -o -iname '*.c' -exec clang-format --dry-run --Werror {} \;
 
 # build compile_commands.json
 CC=clang bazel build //...
@@ -13,8 +12,7 @@ CC=clang bazel run @hedron_compile_commands//:refresh_all
 
 # run linter
 find source/ -type f -iname '*.c' ! -iname '*.template.c' -exec clang-tidy --config-file ./source/.clang-tidy -p ./ {} \;
-find test/ -type f -iname '*.c' -exec clang-tidy --config-file ./test/.clang-tidy -p ./ {} \;
 
 # run code coverate
-CC=gcc bazel coverage --instrumentation_filter //source/... --combined_report=lcov //test/...
+CC=gcc bazel coverage --instrumentation_filter //source/... --noinstrument_test_targets --combined_report=lcov //source/...
 genhtml --branch-coverage --output genhtml "$(bazel info output_path)/_coverage/_coverage_report.dat"
