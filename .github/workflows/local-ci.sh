6e3ea5d4db4a01acda10ce2d162bd3a90600401e
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2024-12-02 07:39:41 -0800

    Move icu4c dependency out to an external repo

--- .github/workflows/local-ci.sh
+++ .github/workflows/local-ci.sh
@@ -7,12 +7,12 @@ rm compile_commands.json
 find source/ -iname '*.h' -o -iname '*.c' -exec clang-format --dry-run --Werror {} \;
 
 # build compile_commands.json
-CC=clang bazel build //source/...
+CC=clang bazel build //...
 CC=clang bazel run @hedron_compile_commands//:refresh_all
 
 # run linter
 find source/ -type f -iname '*.c' ! -iname '*.template.c' -exec clang-tidy --config-file ./source/.clang-tidy -p ./ {} \;
 
 # run code coverate
-CC=gcc bazel coverage --instrumentation_filter //source/... --noinstrument_test_targets --combined_report=lcov //source/...
+CC=gcc bazel coverage --instrumentation_filter //... --noinstrument_test_targets --combined_report=lcov //...
 genhtml --branch-coverage --output genhtml "$(bazel info output_path)/_coverage/_coverage_report.dat"
