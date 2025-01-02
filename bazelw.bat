616419ea890da6203581bfe44459e17d36dd9fd7
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2025-01-02 05:09:45 -0800

    Use bazel wrapper to remove system dependency on bazel
    Gradle uses this awesome wrapper script pattern that removes the system dependency requirement on gradle from the build. This replicates that pattern with bazel and bazelisk. One more step towards removing all system dependency requirements from the build.

---
+++ bazelw.bat
@@ -0,0 +1,29 @@
+@echo off
+
+:: Determine the architecture
+set ARCH=
+for /f "tokens=2 delims==" %%A in ('"wmic os get osarchitecture /value"') do set ARCH=%%A
+
+if "%ARCH%"=="32-bit" (
+    set ARCH=amd64
+) else if "%ARCH%"=="64-bit" (
+    set ARCH=amd64
+) else if "%ARCH%"=="ARM64" (
+    set ARCH=arm64
+) else (
+    echo Unsupported architecture: %ARCH%
+    exit /b 1
+)
+
+:: Construct the path to the Bazelisk binary
+set OS=windows
+set BAZELISK_BINARY=tools\bazelisk-%OS%-%ARCH%.exe
+
+:: Check if the binary exists
+if not exist "%BAZELISK_BINARY%" (
+    echo Bazelisk binary not found: %BAZELISK_BINARY%
+    exit /b 1
+)
+
+:: Run the Bazelisk binary
+"%BAZELISK_BINARY%" %*
--- source/testing/main.c
+++
@@ -1,7 +0,0 @@
-#define TEMPLATE_PARAM_T "Hi!\n"
-#include "foo.template.h"
-
-int main() {
-    foo();
-    return 0;
-}
